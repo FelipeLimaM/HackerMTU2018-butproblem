54c735e9ddfe107827f4a5ed0643b6054e594817
Matheus Jun Ota <matheusota@gmail.com>
2018-04-07 20:01:06 -0300

    filtro por frequencia

--- heatmap/drawmap.py
+++ heatmap/drawmap.py
@@ -7,6 +7,8 @@ gmap = gmplot.GoogleMapPlotter(-23.946528, -46.353571, 13)
 # Get bus stops
 bus_stops_lat = []
 bus_stops_long = []
+scatter_lat = []
+scatter_long = []
 
 with open("../data/RMBS_coord_freq_line_904_bus_8611.txt", "rb") as f:
 	reader = csv.reader(f)
@@ -16,11 +18,16 @@ with open("../data/RMBS_coord_freq_line_904_bus_8611.txt", "rb") as f:
 		if lon >= -46.41:
 			f = int(row[2])
 			
+			if f > 11:
+				scatter_lat.append(lat)
+				scatter_long.append(lon)
+					
 			for i in range(f):
 				bus_stops_lat.append(lat)
 				bus_stops_long.append(lon)
 
 gmap.heatmap(bus_stops_lat, bus_stops_long, radius = 10, maxIntensity = 40)
+gmap.scatter(scatter_lat, scatter_long, 'cornflowerblue', size=100)
 
 # Draw
 gmap.draw("my_map.html")
